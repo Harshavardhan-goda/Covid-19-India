356b3592581f423b7b5fcfbb9247f37351cc6415
harshavardhan <harshavardhan@gmail.com>
2023-04-10 09:02:37 +0000

    get states by using express API's

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const dbPath = path.join(__dirname, "covid19India.db");
+
+let db = null;
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB error ${e.message}`);
+  }
+};
+initializeDbAndServer();
+
+const convertCase1 = (each) => {
+  return {
+    stateId: each.state_id,
+    stateName: each.state_name,
+    population: each.population,
+  };
+};
+//Get states API
+app.get("/states/", async (request, response) => {
+  const getStatesQuery = `
+    SELECT * FROM state;`;
+  const stateArray = await db.all(getStatesQuery);
+  response.send(stateArray.map((each) => convertCase1(each)));
+});
